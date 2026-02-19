54930e11c11917cc1402eac2f81b01aa91d670f1
krdheeraj51 <krdheeraj51@gmail.com>
2026-02-19 22:32:26 +0530

    added for fb 19

--- 2026/February/winterGamesDay14.py
+++ 2026/February/winterGamesDay14.py
@@ -9,7 +9,7 @@
 # "Gentle"	"Safe"	"Safe"	"Safe"
 # "Steep"	"Safe"	"Risky"	"Risky"
 # "Very Steep"	"Safe"	"Risky"	"Risky"
-
+# https://www.freecodecamp.org/learn/daily-coding-challenge/2026-02-19
 def avalanche_risk(snow_depth, slope):
     if snow_depth == "Shallow":
         return "Safe"
